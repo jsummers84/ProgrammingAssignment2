0374526d94f2290fd947eeb65f188765f81a5f33
jsummers84 <jasminessummers@gmail.com>
2019-07-27 17:09:08 -0400

    Updated with comments

--- jsummers Programming Assignment2 Solution.R
+++ jsummers Programming Assignment2 Solution.R
@@ -1,3 +1,5 @@
+##This function creates a special "matrix" object that can cache its inverse.
+
 makeCacheMatrix <- function(x=matrix()){
   i <- NULL
   set <- function(y){
@@ -13,6 +15,8 @@ makeCacheMatrix <- function(x=matrix()){
        getinverse = getinverse)
 }
 
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
+
 cacheSolve <- function (x, ...){
   i <- x$getinverse()
   if (!is.null(i)){
@@ -24,3 +28,17 @@ cacheSolve <- function (x, ...){
   x$setinverse(i)
   i
 }
+
+##Testing the solution:
+A <- matrix(c(1,2,3,4),2,2)
+> A1 <- makeCacheMatrix(A)
+> cacheSolve(A1)
+[,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+> cacheSolve(A1)
+getting cached data
+[,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+##cacheSolve was able to save the inversion of the matrix without having to repeat the computation
